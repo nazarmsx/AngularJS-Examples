be2ad7147115fe7c0aed38860bedc47b5e609c27
Nazar <nazarmsx@ukr.net>
2015-05-28 13:13:56 +0300

    Home controller changed.

--- app/app.js
+++ app/app.js
@@ -1,15 +1,55 @@
 var app = angular.module('myApp', ['ngRoute']);
 
-app.controller('loginController', function ($scope) {
-                $scope.password = '';
-                $scope.email = '';
-                $scope.onLogin=function(){console.log('Login!')};
-            });
-app.controller('registerController', function ($scope) {
-                });
-app.controller('homeController', function ($scope) {
-             });
 
+app.controller('userCtrl', function($scope,$http) {
+$scope.name = '';
+$scope.email = '';
+$scope.telephone = '';
+$scope.street = '';
+$scope.city = '';
+$scope.state = '';
+$scope.zip = '';
+
+
+$http.get("./app/customers.json")
+  .success(function (response) {$scope.users = response.records;});
+  
+$scope.edit = true;
+$scope.error = false;
+$scope.incomplete = false; 
+$scope.editUser = function(id) {
+  if (id == 'new') {
+    $scope.edit = true;
+    $scope.incomplete = true;
+    $scope.fName = '';
+    $scope.lName = '';
+    } else {
+    //$scope.edit = false;
+    for(var i=0;i<$scope.users.length;i++)
+    {
+    if($scope.users[i].email=id)
+    {
+    var user=$scope.users[i];
+    $scope.name = user.name;
+    $scope.email = user.email;
+    $scope.telephone = user.telephone;
+    $scope.street = user.street;
+    $scope.city = user.city;
+    $scope.state = user.state;
+    $scope.zip = user.zip;
+    }
+    break;
+    }
+    
+  }
+};
+$scope.saveCustomerInfo=function()
+{
+alert($scope.name);   
+};
+
+
+})
 
 app.config(function($routeProvider) {
 		$routeProvider
@@ -17,18 +57,7 @@ app.config(function($routeProvider) {
 			// route for the home page
 			.when('/', {
 				templateUrl : './views/home.html',
-				controller  : 'homeController'
-			})
-
-			// route for the about page
-			.when('/login', {
-				templateUrl : './views/login.html',
-				controller  : 'loginController'
-			})
-
-			// route for the contact page
-			.when('/register', {
-				templateUrl : './views/register.html',
-				controller  : 'registerController'
+				controller  : 'userCtrl'
 			});
+			
 	});
